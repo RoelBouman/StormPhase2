424af0bf0837d15ed92e97b2465fbd44efca287b
Roel <Bouman>
2022-03-18 16:14:43 +0100

    Added setup for IF evaluation

--- run_methods.py
+++ run_methods.py
@@ -13,6 +13,7 @@ import numpy as np
 
 from sklearn.ensemble import IsolationForest
 from sklearn.model_selection import ParameterGrid
+from sklearn.metrics import fbeta_score
 
 from src.isolation_forest import threshold_scores
 
@@ -52,7 +53,7 @@ for pickle_file in os.listdir(pickle_train_file_folder):
         #hyperparameter_string = re.sub(r"[^a-zA-Z0-9_ ]","",str(hyperparameter_settings))
         hyperparameter_string = str(hyperparameter_settings)
         
-        result_file_path = os.path.join(result_folder, method_name, hyperparameter_string, substation_name+".pickle")
+        result_file_path = os.path.join(result_folder, "X_train", method_name, hyperparameter_string, substation_name+".pickle")
         
         if os.path.exists(result_file_path):
             pass
@@ -65,8 +66,8 @@ for pickle_file in os.listdir(pickle_train_file_folder):
             model.fit(X)
             y_scores = model.decision_function(X)
             
-            if not os.path.exists(os.path.join(result_folder, method_name, hyperparameter_string)):
-                os.makedirs(os.path.join(result_folder, method_name, hyperparameter_string))
+            if not os.path.exists(os.path.join(result_folder, "X_train", method_name, hyperparameter_string)):
+                os.makedirs(os.path.join(result_folder, "X_train", method_name, hyperparameter_string))
             
             with open(result_file_path, 'wb') as handle:
                 pickle.dump(y_scores, handle)
@@ -80,3 +81,44 @@ method_name = "IF"
 
 for hyperparameter_settings in hyperparameter_list:
     hyperparameter_string = str(hyperparameter_settings)
+    
+    # check if all results have actually been calculated
+    
+    station_score_folder = os.path.join(result_folder, "X_train", method_name, hyperparameter_string)
+    X_file_folder = os.path.join(data_folder, "X_train_preprocessed_pickle")
+    
+    station_score_files = os.listdir(station_score_folder)
+    
+    X_files = os.listdir(X_file_folder)
+    
+    if station_score_files == X_files:
+        y_scores = []
+        y_true = []
+        
+        event_lengths = []
+        
+        for station_score_file in station_score_files:
+            
+            y_scores.append(pickle.load(open(os.path.join(station_score_folder, station_score_file), 'rb')))
+            #y_score = pickle.load(open(os.path.join(station_score_folder, station_score_file), 'rb'))
+            data = pickle.load(open(os.path.join(X_file_folder, station_score_file), 'rb'))
+            
+            y_true.append(data["y"])
+            
+            event_lengths.append(data["lengths"])
+            
+        y_scores_combined = np.concatenate(y_scores)
+        y_true_combined = np.concatenate(y_true)
+        
+        #remove samples where y_true == 5
+        
+        y_scores_filtered = y_scores_combined[y_true_combined != 5]
+        y_true_filtered = y_true_combined[y_true_combined != 5]
+        
+        #Write evaluation function working per length
+        
+        #Should we handle suspect missing data at evaluation or calculation time. Linda did it at evaluation, but this might lead to problems with time series methods such as binseg
+           
+    else:
+        Warning("Not all scores for this hyperparameter setting have been calculated yet. Skipping...")
+        
