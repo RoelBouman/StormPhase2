2e3fd4d9e79080bc259ca915201224894fcb1a4c
Roel Bouman <r.bouman@science.ru.nl>
2023-10-31 14:23:22 +0100

    Finished all figures/tables for preprocessing

--- produce_paper_figures.py
+++ produce_paper_figures.py
@@ -16,6 +16,10 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+from src.plot_functions import plot_S_original, plot_BU_original
+
 sns.set()
 
 #%% Data loading
@@ -24,6 +28,12 @@ data_folder = "data"
 result_folder = "results"
 intermediates_folder = "intermediates"
 model_folder = "saved_models"
+
+table_folder = "Tables"
+figure_folder = "Figures"
+
+os.makedirs(table_folder, exist_ok=True)
+os.makedirs(figure_folder, exist_ok=True)
 
 score_folder = os.path.join(result_folder, "scores")
 predictions_folder = os.path.join(result_folder, "predictions")
@@ -40,10 +50,51 @@ all_dataset_names = [train_name, test_name, validation_name]
 #%% Visualize/tabularize input data and preprocessing
 
 #measurement_example.pdf
+station_ID = "019"
+
+n_xlabels = 10
+
+train_IDs = os.listdir(os.path.join(data_folder, "Train", "X"))
+test_IDs = os.listdir(os.path.join(data_folder, "Test", "X"))
+validation_IDs = os.listdir(os.path.join(data_folder, "Validation", "X"))
+
+all_station_IDs = train_IDs + test_IDs + validation_IDs
+
+train_ID_dict = {ID.replace(".csv", ""): "Train" for ID in train_IDs}
+test_ID_dict = {ID.replace(".csv", ""): "Test" for ID in test_IDs}
+validation_ID_dict = {ID.replace(".csv", ""): "Validation" for ID in validation_IDs}
+
+#fastest dict merge: https://stackoverflow.com/questions/1781571/how-to-concatenate-two-dictionaries-to-create-a-new-one
+station_dataset_dict = dict(train_ID_dict, **test_ID_dict)
+station_dataset_dict.update(validation_ID_dict)
+
+X_df = pd.read_csv(os.path.join(data_folder, station_dataset_dict[station_ID], "X", station_ID + ".csv"))
+y_df = pd.read_csv(os.path.join(data_folder, station_dataset_dict[station_ID], "y", station_ID + ".csv"))
 
 
-#event_length distribution.pdf
-#Show as histogram
+fig = plt.figure(figsize=(30,16)) # add DPI=300+ in case some missing points don't show up'    
+plt.title("Station: " + station_ID, fontsize=60)
+
+
+plot_S_original(X_df, label="S original")
+plot_BU_original(X_df, label="BU original")
+
+plt.legend(fontsize=30)
+
+plt.yticks(fontsize=30)
+plt.ylabel("S", fontsize=30)
+
+ticks = np.linspace(0,len(X_df["S_original"])-1, n_xlabels, dtype=int)
+plt.xticks(ticks=ticks, labels=X_df["M_TIMESTAMP"].iloc[ticks], rotation=45, fontsize=30)
+plt.xlim((0, len(X_df)))
+
+plt.tight_layout()
+
+plt.savefig(os.path.join(figure_folder, "measurement_example.pdf"), format="pdf")
+plt.savefig(os.path.join(figure_folder, "measurement_example.png"), format="png")
+plt.show()
+#event_length_distribution.pdf, event_length_stats.tex
+#Show as histogram and stats table
 lengths = []
 for split_name in all_dataset_names:
     lengths_path = os.path.join(intermediates_folder, "event_length_pickles", split_name)
@@ -56,9 +107,43 @@ concat_lengths = np.concatenate(lengths)
 
 unique_lengths, length_counts = np.unique(concat_lengths, return_counts=True)
 
+
+unique_lengths = unique_lengths[1:]
+length_counts = length_counts[1:]
+
 normalized_length_counts = []
 for unique_length, length_count in zip(unique_lengths, length_counts):#skip 0
     normalized_length_counts.append(length_count/unique_length)
+    
+normalized_length_count_per_cutoff = {str(cutoff):0 for cutoff in all_cutoffs}
+length_count_per_cutoff = {str(cutoff):0 for cutoff in all_cutoffs}
+
+for unique_length, normalized_length_count, length_count in zip(unique_lengths,normalized_length_counts, length_counts):
+    for cutoff in all_cutoffs:
+        if unique_length >= cutoff[0] and unique_length < cutoff[1]: 
+            normalized_length_count_per_cutoff[str(cutoff)] += normalized_length_count
+            length_count_per_cutoff[str(cutoff)] += length_count
+            
+normalized_length_count_per_cutoff = {key:[value] for key, value in normalized_length_count_per_cutoff.items()}
+length_count_per_cutoff = {key:[value] for key, value in length_count_per_cutoff.items()}
+
+event_length_stats = pd.concat([pd.DataFrame(normalized_length_count_per_cutoff), pd.DataFrame(length_count_per_cutoff)])
+event_length_stats.index = ["#events per cutoff", "#label $1$ per cutoff"]
+event_length_stats.to_latex(buf=os.path.join(table_folder, "event_length_stats.tex"))
+
+lengths_to_hist = []
+for unique_length, normalized_length_count in zip(unique_lengths, normalized_length_counts):
+    lengths_to_hist += [unique_length]*int(normalized_length_count)
+
+plt.figure()
+plt.hist(lengths_to_hist, bins=100)
+plt.yscale('log')
+plt.xlabel("Event Length (#data points)")
+plt.ylabel("Counts (log)")
+plt.savefig(os.path.join(figure_folder, "event_length_distribution.pdf"), format="pdf")
+plt.savefig(os.path.join(figure_folder, "event_length_distribution.png"), format="png")
+plt.show()
+
 #%% Visualize threshold optimization strategy
 
 
