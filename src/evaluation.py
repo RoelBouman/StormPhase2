826f535057e452064e8c1876409a841eea8edc6b
Roel Bouman <r.bouman@science.ru.nl>
2024-02-06 12:03:31 +0100

    Added first setup for bootstrapping

--- src/evaluation.py
+++ src/evaluation.py
@@ -7,6 +7,7 @@ Created on Thu Jun 29 14:02:32 2023
 """
 import numpy as np
 import pandas as pd
+from numba import jit
 
 from sklearn.metrics import fbeta_score, precision_score, recall_score, roc_auc_score
 
@@ -85,3 +86,34 @@ def calculate_unsigned_absolute_and_relative_stats(X_dfs, y_dfs, y_pred_dfs, loa
 
     return absolute_min_differences, absolute_max_differences, relative_min_differences, relative_max_differences
 
+
+# @jit(nopython=True)
+def calculate_bootstrap_stats(y_dfs, y_scores_dfs, y_pred_dfs, label_filters_for_all_cutoffs, beta,  bootstrap_iterations=10000):
+    
+    n_cutoffs = len(label_filters_for_all_cutoffs[0])
+    n_dfs = len(y_dfs)
+    
+    PRFAUC_table_per_iteration = np.zeros((n_cutoffs, 4, bootstrap_iterations))
+    metric_per_iteration = np.zeros((bootstrap_iterations,))
+    
+    for i in range(bootstrap_iterations):
+        
+        bootstrap_indices  = np.random.choice(n_dfs, size=n_dfs)
+        
+        y_dfs_bootstrapped = [y_dfs[j] for j in bootstrap_indices]
+        #y_scores_dfs_bootstrapped = [y_scores_dfs[j] for j in bootstrap_indices]
+        y_pred_dfs_bootstrapped = [y_pred_dfs[j] for j in bootstrap_indices]
+        label_filters_for_all_cutoffs_bootstrapped = [label_filters_for_all_cutoffs[j] for j in bootstrap_indices]
+        
+        
+        PRFAUC_table_per_iteration[:,:,i] = calculate_PRFAUC_table(y_dfs_bootstrapped, y_pred_dfs_bootstrapped, label_filters_for_all_cutoffs_bootstrapped, beta)
+        metric_per_iteration[i] = cutoff_averaged_f_beta(y_dfs_bootstrapped, y_pred_dfs_bootstrapped, label_filters_for_all_cutoffs_bootstrapped, beta)
+        
+        
+    metric_mean = np.mean(metric_per_iteration)
+    metric_std = np.std(metric_per_iteration)
+    
+    PRFAUC_table_mean = np.mean(PRFAUC_table_per_iteration, axis=2)
+    PRFAUC_table_std = np.std(PRFAUC_table_per_iteration, axis=2)
+    
+    return metric_mean, metric_std, PRFAUC_table_mean, PRFAUC_table_std
