08c85cb3bc57c1ce199990ef4ff62758792962b1
Roel Bouman <r.bouman@science.ru.nl>
2024-02-08 17:42:25 +0100

    Intermediate work on optimized bootstrapping

--- src/evaluation.py
+++ src/evaluation.py
@@ -9,9 +9,9 @@ import numpy as np
 import pandas as pd
 from numba import njit
 
-from sklearn.metrics import fbeta_score, precision_score, recall_score, roc_auc_score
+from sklearn.metrics import fbeta_score, precision_score, recall_score, roc_auc_score, confusion_matrix
 
-from .helper_functions import filter_label_and_predictions_to_array
+from .helper_functions import filter_label_and_predictions_to_array, filter_label_and_predictions
 
 
 def f_beta(precision, recall, beta=1, eps=np.finfo(float).eps):
@@ -86,43 +86,43 @@ def calculate_unsigned_absolute_and_relative_stats(X_dfs, y_dfs, y_pred_dfs, loa
 
     return absolute_min_differences, absolute_max_differences, relative_min_differences, relative_max_differences
 
+@njit
+def bootstrap_stats_per_confmat_array(bootstrap_samples, confmat_per_station):
+    for indices in bootstrap_samples:
+        confmats = [confmat_per_station[i] for i in indices]
+        
+        confmat_sum = np.array([0,0,0,0])
+        for confmat in confmats:
+            confmat_sum += confmat
+            
+        
+
 def calculate_bootstrap_stats(y_dfs, y_pred_dfs, label_filters_for_all_cutoffs, beta, bootstrap_iterations=10000):
-    y_arrays = df_list_to_array_list(y_dfs, column="label")
-    y_pred_arrays = df_list_to_array_list(y_pred_dfs, column="label")
-    label_array_filters_for_all_cutoffs = label_filters_to_array(label_filters_for_all_cutoffs)
+
+    all_cutoffs = label_filters_for_all_cutoffs[0].keys()
     
-    metric_mean, metric_std, PRFAUC_table_mean, PRFAUC_table_std = _calculate_bootstrap_stats(y_arrays, y_pred_arrays, label_array_filters_for_all_cutoffs, beta, bootstrap_iterations=bootstrap_iterations)
+    n_cutoffs = len(all_cutoffs)
+    n_dfs = len(y_dfs)
+
+    confmat_per_station_per_cutoff = {}
+    
+    for cutoffs in all_cutoffs:
+        filtered_y, filtered_y_preds = filter_label_and_predictions(y_dfs, y_pred_dfs, label_filters_for_all_cutoffs, cutoffs)
+        
+        confmat_per_station = np.zeros((len(y_dfs),4))
+        for i, (y, y_pred) in enumerate(zip(filtered_y, filtered_y_preds)):
+            confmat_per_station[i,:] = confusion_matrix(y, y_pred, label=[0,1]).ravel() #TODO: cast to int32
+        
+        confmat_per_station_per_cutoff[str(cutoffs)] = confmat_per_station
+        
+    bootstrap_samples = nnp.random.choice(np.arange(n_dfs, dtype=np.int32), size=(bootstrap_iterations, n_dfs))
+    
+    for cutoffs in all_cutoffs:
+            _,_,_,_ = bootstrap_stats_per_confmat_array(bootstrap_samples, confmat_per_station)
 
     return metric_mean, metric_std, PRFAUC_table_mean, PRFAUC_table_std
 
 
-
-def calculate_PRFAUC_table_from_arrays(y_arrays, y_pred_arrays, label_array_filters_for_all_cutoffs, beta):
-    n_cutoffs = label_array_filters_for_all_cutoffs[0].shape[1]
-
-    precisions = np.zeros((n_cutoffs,))
-    recalls = np.zeros((n_cutoffs,))
-    fbetas = np.zeros((n_cutoffs,))
-    AUCs = np.zeros((n_cutoffs,))
-    
-    for i in range(n_cutoffs):
-        filtered_y, filtered_y_preds = filter_array_label_and_predictions_to_array(y_arrays, y_pred_arrays, label_array_filters_for_all_cutoffs, cutoff=i)
-        
-        
-        precisions.append(precision_score(filtered_y, filtered_y_preds))
-        recalls.append(recall_score(filtered_y, filtered_y_preds))
-        fbetas.append(fbeta_score(filtered_y, filtered_y_preds, beta=beta))
-        AUCs.append(roc_auc_score(filtered_y, filtered_y_preds))
-        
-    PRFAUC_table = pd.DataFrame(index=all_cutoffs)
-    PRFAUC_table.index.name = "Cutoffs"
-    
-    PRFAUC_table["precision"] = precisions
-    PRFAUC_table["recall"] = recalls
-    PRFAUC_table["F"+str(beta)] = fbetas
-    PRFAUC_table["ROC/AUC"] = AUCs
-    
-    return PRFAUC_table
 
 
 
--- src/helper_functions.py
+++ src/helper_functions.py
@@ -7,14 +7,29 @@ import numpy as np
 # import pandas as pd
 # from numba import njit
 
-def filter_dfs_to_array(dfs, df_filters):
+def filter_dfs(dfs, df_filters):
     #filter defines what to exclude, so, True is excluded
     filtered_dfs = [df[np.logical_not(df_filter)] for df, df_filter in zip(dfs, df_filters)]
     
+    return filtered_dfs
+
+def filter_dfs_to_array(dfs, df_filters):
+    #filter defines what to exclude, so, True is excluded
+    filtered_dfs = filter_dfs(dfs, df_filters)
+    
     return np.concatenate(filtered_dfs)
 
-def filter_label_and_predictions_to_array(y_dfs, y_preds_dfs, label_filters_for_all_cutoffs, cutoffs):
-    df_filters = [filter_df[str(cutoffs)] for filter_df in label_filters_for_all_cutoffs]
+def filter_label_and_predictions(y_dfs, y_preds_dfs, label_filters_for_all_cutoffs, cutoff):
+    df_filters = [filter_df[str(cutoff)] for filter_df in label_filters_for_all_cutoffs]
+    y_label_dfs = [df["label"] for df in y_dfs]
+    
+    filtered_y_labels = filter_dfs(y_label_dfs, df_filters)
+    filtered_y_preds = filter_dfs(y_preds_dfs, df_filters)
+    
+    return filtered_y_labels, filtered_y_preds
+
+def filter_label_and_predictions_to_array(y_dfs, y_preds_dfs, label_filters_for_all_cutoffs, cutoff):
+    df_filters = [filter_df[str(cutoff)] for filter_df in label_filters_for_all_cutoffs]
     y_label_dfs = [df["label"] for df in y_dfs]
     
     filtered_y_labels = filter_dfs_to_array(y_label_dfs, df_filters)
@@ -31,23 +46,3 @@ def filter_label_and_scores_to_array(y_dfs, y_scores_dfs, label_filters_for_all_
     filtered_y_scores = filter_dfs_to_array(y_scores_dfs, df_filters).squeeze()
     
     return filtered_y_labels, filtered_y_scores
-
-# def df_list_to_array_list(df_list, column):
-#     array_list = [df[column].to_numpy(dtype=np.int32) for df in df_list]
-
-#     return array_list
-
-# def label_filters_to_array(label_filters_for_all_cutoffs):
-#     filters_array_list = [pd.DataFrame(filters).to_numpy(dtype=np.bool_) for filters in label_filters_for_all_cutoffs]
-    
-#     return filters_array_list
-
-# @njit
-# def filter_array_label_and_predictions_to_array(y_arrays, y_pred_arrays, label_array_filters_for_all_cutoffs=list(np.array([[]])), cutoff_index=0):
-#     y_arrays_cat = np.concatenate(y_arrays)
-#     y_pred_arrays_cat = np.concatenate(y_pred_arrays)
-#     label_filter_arrays = np.concatenate(label_array_filters_for_all_cutoffs, axis=0)
-        
-#     label_filter = label_filter_arrays[:,cutoff_index]
-    
-#     return y_arrays_cat[label_filter], y_pred_arrays_cat[label_filter]
