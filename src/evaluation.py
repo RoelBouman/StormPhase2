0f6bbe2b45150e228946dd080d17ffe8815df57e
Roel Bouman <r.bouman@science.ru.nl>
2024-02-09 13:06:42 +0100

    Finished first version of fast bootstrapping for PRF stats

--- src/evaluation.py
+++ src/evaluation.py
@@ -7,17 +7,17 @@ Created on Thu Jun 29 14:02:32 2023
 """
 import numpy as np
 import pandas as pd
-from numba import njit
+from numba import njit, prange
 
 from sklearn.metrics import fbeta_score, precision_score, recall_score, roc_auc_score, confusion_matrix
 
 from .helper_functions import filter_label_and_predictions_to_array, filter_label_and_predictions
 
-
 def f_beta(precision, recall, beta=1, eps=np.finfo(float).eps):
     
     return(1+beta**2) * (precision*recall)/((beta**2*precision)+recall+eps)
 
+@njit
 def f_beta_from_confmat(fp, tp, fn, beta=1, eps=np.finfo(float).eps):
     
     return (1+beta**2) * (tp) / ((1+beta**2)*tp + (beta**2)*fn + fp + eps)
@@ -86,16 +86,41 @@ def calculate_unsigned_absolute_and_relative_stats(X_dfs, y_dfs, y_pred_dfs, loa
 
     return absolute_min_differences, absolute_max_differences, relative_min_differences, relative_max_differences
 
-@njit
-def bootstrap_stats_per_confmat_array(bootstrap_samples, confmat_per_station):
-    for indices in bootstrap_samples:
-        confmats = [confmat_per_station[i] for i in indices]
+@njit(parallel=True)
+def bootstrap_stats_per_confmat_array(bootstrap_samples, confmat_per_station, beta, eps=np.finfo(float).eps):
+    
+    bootstrap_iterations = bootstrap_samples.shape[0]
+    
+    metrics = np.zeros((bootstrap_iterations, 3), dtype=np.float64)
+    #in order: precision, recall, Fbeta
+    
+    for i in prange(bootstrap_iterations):
+        indices = bootstrap_samples[i,:]
+        
+        confmats = [confmat_per_station[k] for k in indices]
         
         confmat_sum = np.array([0,0,0,0])
         for confmat in confmats:
             confmat_sum += confmat
             
+        tn, fp, fn, tp = confmat_sum
         
+        metrics[i,0] = tp/(tp+fp)
+        metrics[i,1] = tp/(tp+fn)
+        metrics[i,2] = (1+beta**2) * (tp) / ((1+beta**2)*tp + (beta**2)*fn + fp + eps)
+        
+    mean_metrics = np.zeros((3,), dtype=np.float64)
+    mean_metrics[0] = np.mean(metrics[:,0])
+    mean_metrics[1] = np.mean(metrics[:,1])
+    mean_metrics[2] = np.mean(metrics[:,2])
+    
+    std_metrics = np.zeros((3,), dtype=np.float64)
+    std_metrics[0] = np.std(metrics[:,0])
+    std_metrics[1] = np.std(metrics[:,1])
+    std_metrics[2] = np.std(metrics[:,2])
+    
+    return mean_metrics, std_metrics
+
 
 def calculate_bootstrap_stats(y_dfs, y_pred_dfs, label_filters_for_all_cutoffs, beta, bootstrap_iterations=10000):
 
@@ -104,23 +129,39 @@ def calculate_bootstrap_stats(y_dfs, y_pred_dfs, label_filters_for_all_cutoffs, 
     n_cutoffs = len(all_cutoffs)
     n_dfs = len(y_dfs)
 
-    confmat_per_station_per_cutoff = {}
+    #confmat_per_station_per_cutoff = {}
     
-    for cutoffs in all_cutoffs:
+    bootstrap_samples = np.random.choice(np.arange(n_dfs, dtype=np.int32), size=(bootstrap_iterations, n_dfs))
+    
+    table_mean = np.zeros((n_cutoffs,3))
+    table_std = np.zeros((n_cutoffs,3))
+    
+    for i, cutoffs in enumerate(all_cutoffs):
         filtered_y, filtered_y_preds = filter_label_and_predictions(y_dfs, y_pred_dfs, label_filters_for_all_cutoffs, cutoffs)
         
-        confmat_per_station = np.zeros((len(y_dfs),4))
-        for i, (y, y_pred) in enumerate(zip(filtered_y, filtered_y_preds)):
-            confmat_per_station[i,:] = confusion_matrix(y, y_pred, label=[0,1]).ravel() #TODO: cast to int32
+        confmat_per_station = np.zeros((len(y_dfs),4), dtype=np.int32)
+        for j, (y, y_pred) in enumerate(zip(filtered_y, filtered_y_preds)):
+            confmat_per_station[j,:] = confusion_matrix(y, y_pred, labels=[0,1]).ravel()
         
-        confmat_per_station_per_cutoff[str(cutoffs)] = confmat_per_station
+        #confmat_per_station_per_cutoff[str(cutoffs)] = confmat_per_station
         
-    bootstrap_samples = nnp.random.choice(np.arange(n_dfs, dtype=np.int32), size=(bootstrap_iterations, n_dfs))
-    
-    for cutoffs in all_cutoffs:
-            _,_,_,_ = bootstrap_stats_per_confmat_array(bootstrap_samples, confmat_per_station)
+        table_mean[i,:], table_std[i,:] = bootstrap_stats_per_confmat_array(bootstrap_samples, confmat_per_station, beta)
 
-    return metric_mean, metric_std, PRFAUC_table_mean, PRFAUC_table_std
+    PRF_mean_table = pd.DataFrame(index=all_cutoffs)
+    PRF_mean_table.index.name = "Cutoffs"
+    
+    PRF_mean_table["precision"] = table_mean[:,0]
+    PRF_mean_table["recall"] = table_mean[:,1]
+    PRF_mean_table["F"+str(beta)] = table_mean[:,2]
+    
+    PRF_std_table = pd.DataFrame(index=all_cutoffs)
+    PRF_std_table.index.name = "Cutoffs"
+    
+    PRF_std_table["precision"] = table_std[:,0]
+    PRF_std_table["recall"] = table_std[:,1]
+    PRF_std_table["F"+str(beta)] = table_std[:,2]
+
+    return PRF_mean_table, PRF_std_table
 
 
 
