90a04811117eff880e78aa58e4ed411129da2475
Roel Bouman <r.bouman@science.ru.nl>
2024-02-29 09:23:32 +0100

    Added initial setup to plotting of bootstrap results

--- produce_paper_figures.py
+++ produce_paper_figures.py
@@ -23,7 +23,7 @@ from hashlib import sha256
 
 
 from src.plot_functions import plot_S_original, plot_BU_original, plot_predictions
-from src.io_functions import load_PRFAUC_table, load_batch, load_dataframe_list
+from src.io_functions import load_table, load_batch, load_dataframe_list
 
 from src.methods import SingleThresholdStatisticalProcessControl
 from src.methods import DoubleThresholdStatisticalProcessControl
@@ -34,6 +34,7 @@ from src.methods import DoubleThresholdBinarySegmentation
 
 from src.methods import StackEnsemble
 from src.methods import NaiveStackEnsemble
+from src.methods import SequentialEnsemble
 
 from src.preprocess import preprocess_per_batch_and_write
 
@@ -63,11 +64,11 @@ train_name = "Train"
 test_name = "Test"
 validation_name = "Validation"
 
-all_dataset_names = [train_name, test_name, validation_name]
+all_dataset_names = [train_name, validation_name, test_name]
 
 #%% connect to database
 
-DBFILE = "experiment_results.db"
+DBFILE = dataset+"_experiment_results.db"
 database_exists = os.path.exists(DBFILE)
 
 db_connection = sqlite3.connect(DBFILE) # implicitly creates DBFILE if it doesn't exist
@@ -186,12 +187,15 @@ plt.show()
 
 
 #load model:
-preprocessing_hash = "10cab9fc324db7a2fd5d8674c71edb68908b5e572ffa442d201eb0ca0aa288e1"
+preprocessing_hash = "ef19085e70a2b043dd00e10361154f3ec54122c056f0a5236099c900ff889eff"
 hyperparameter_hash = "863c7a1a49f110ada1d11bf21549b9f60f53c72042a80a36a0969583a18d42e1"
 
 db_result = db_cursor.execute("SELECT method_hyperparameters FROM experiment_results WHERE preprocessing_hash='{}' AND hyperparameter_hash='{}'".format(preprocessing_hash, hyperparameter_hash)).fetchone()[0]
 
 hyperparameters = jsonpickle.loads(db_result)
+
+
+# plt.rcParams['text.usetex'] = True
 
 from src.evaluation import f_beta
 from src.methods import SingleThresholdStatisticalProcessControl
@@ -201,7 +205,7 @@ def score_function(precision, recall):
 
 
 model = SingleThresholdStatisticalProcessControl(model_folder, preprocessing_hash, **hyperparameters, score_function=score_function)
-model.calculate_and_set_thresholds(all_cutoffs, score_function)
+model.calculate_and_set_thresholds(all_cutoffs)
 
 plt.figure()
 
@@ -218,14 +222,15 @@ plt.plot(thresholds, np.mean(model.scores, axis=1), label="average", color=sns.c
 plt.axvline(x = model.optimal_threshold, color = sns.color_palette()[3], linestyle="--", label = 'optimal threshold')
 
 plt.legend()
-plt.xlabel("Threshold")
-plt.ylabel("F1.5")
+plt.xlabel(r"Threshold ($\theta$)")
+plt.ylabel(r"F$1.5_{average}$")
 plt.xlim((0, 4)) #hardcoded
 plt.savefig(os.path.join(figure_folder, "threshold_optimization.pdf"), format="pdf")
 plt.savefig(os.path.join(figure_folder, "threshold_optimization.png"), format="png")
 plt.show()
 
-#%% summarize results on validation set for each method
+# plt.rcParams['text.usetex'] = False
+#%% summarize results on test set for each method
 #show for each method:
     # validation average F1.5
     # validation F1.5 for each cutoff/category
@@ -233,108 +238,161 @@ plt.show()
     # best preprocessing hyperparameters (optional?)
     # best hyperparameters (optional?)
 
-#TODO: Technically, we shouldn't select MAX(metric) from Validation, but those hashes where MAX(metric) in Test (if we do 1 run over all hyperparameters, this is moot, but if we keep updating this might spell trouble)
-validation_results = db_cursor.execute("SELECT method, preprocessing_hyperparameters, method_hyperparameters, MAX(metric), preprocessing_hash, hyperparameter_hash FROM experiment_results WHERE which_split='Validation' GROUP BY method").fetchall()
-validation_results_and_metadata_df = pd.DataFrame(validation_results)
-validation_results_and_metadata_df.columns = ["Method", "Preprocessing Hyperparameters", "Method Hyperparameters", "Average F1.5", "preprocessing_hash", "hyperparameter_hash"]
-best_hyperparameter_df = validation_results_and_metadata_df.iloc[:,:3]
-best_hyperparameter_df.set_index("Method", inplace=True)
+# #TODO: Technically, we shouldn't select MAX(metric) from Validation, but those hashes where MAX(metric) in Test (if we do 1 run over all hyperparameters, this is moot, but if we keep updating this might spell trouble)
+# validation_results = db_cursor.execute("SELECT method, preprocessing_hyperparameters, method_hyperparameters, MAX(metric), preprocessing_hash, hyperparameter_hash FROM experiment_results WHERE which_split='Test' GROUP BY method").fetchall()
+# validation_results_and_metadata_df = pd.DataFrame(validation_results)
+# validation_results_and_metadata_df.columns = ["Method", "Preprocessing Hyperparameters", "Method Hyperparameters", "Average F1.5", "preprocessing_hash", "hyperparameter_hash"]
+# best_hyperparameter_df = validation_results_and_metadata_df.iloc[:,:3]
+# best_hyperparameter_df.set_index("Method", inplace=True)
 
-for i in range(best_hyperparameter_df.shape[0]):
-    #Treat ensembles differently:
+# for i in range(best_hyperparameter_df.shape[0]):
+#     #Treat ensembles differently:
     
-    hyperparameters = jsonpickle.loads(best_hyperparameter_df["Method Hyperparameters"].iloc[i])
-    if "method_classes" in hyperparameters: #if dict has method_classes, the method is an ensemble method
-        method_dict = hyperparameters
-        hyperparameter_strings = []
-        for method, hyperparameters in zip(method_dict["method_classes"], method_dict["method_hyperparameter_dict_list"]):
-            method_name = method("temp","temp").method_name
-            hyperparameter_strings.append(method_name + ":" + str(hyperparameters))
-        best_hyperparameter_df["Method Hyperparameters"].iloc[i] = "\n".join(hyperparameter_strings)
-    else:
-        hyperparameters.pop("used_cutoffs", None)
-        best_hyperparameter_df["Method Hyperparameters"].iloc[i] = str(hyperparameters)
-    preprocessing_hyperparameters = jsonpickle.loads(best_hyperparameter_df["Preprocessing Hyperparameters"].iloc[i])
-    best_hyperparameter_df["Preprocessing Hyperparameters"].iloc[i] = str(preprocessing_hyperparameters)
+#     hyperparameters = jsonpickle.loads(best_hyperparameter_df["Method Hyperparameters"].iloc[i])
+#     if "method_classes" in hyperparameters: #if dict has method_classes, the method is an ensemble method
+#         method_dict = hyperparameters
+#         hyperparameter_strings = []
+#         for method, hyperparameters in zip(method_dict["method_classes"], method_dict["method_hyperparameter_dict_list"]):
+#             method_name = method("temp","temp").method_name
+#             hyperparameter_strings.append(method_name + ":" + str(hyperparameters))
+#         best_hyperparameter_df["Method Hyperparameters"].iloc[i] = "\n".join(hyperparameter_strings)
+#     else:
+#         hyperparameters.pop("used_cutoffs", None)
+#         best_hyperparameter_df["Method Hyperparameters"].iloc[i] = str(hyperparameters)
+#     preprocessing_hyperparameters = jsonpickle.loads(best_hyperparameter_df["Preprocessing Hyperparameters"].iloc[i])
+#     best_hyperparameter_df["Preprocessing Hyperparameters"].iloc[i] = str(preprocessing_hyperparameters)
     
-with pd.option_context("display.max_colwidth", None):
-    best_hyperparameter_df.to_latex(buf=os.path.join(table_folder, "best_hyperparameters.tex"), multirow=True)
+# with pd.option_context("display.max_colwidth", None):
+#     best_hyperparameter_df.to_latex(buf=os.path.join(table_folder, "best_hyperparameters.tex"), multirow=True)
 
-#extended results:
-full_validation_results = []
-for i, row in validation_results_and_metadata_df.iterrows():
-    #load PRFAUC table:
-    method_name = row["Method"]
-    preprocessing_hash = row["preprocessing_hash"]
-    hyperparameter_hash = row["hyperparameter_hash"]
-    PRFAUC_table_path = os.path.join(metric_folder, "PRFAUC_table", "Validation", method_name, preprocessing_hash)
-    PRFAUC_table = load_PRFAUC_table(PRFAUC_table_path, hyperparameter_hash)
-    PRFAUC_table.loc["Average", :] = PRFAUC_table.mean()
+# #extended results:
+# full_validation_results = []
+# for i, row in validation_results_and_metadata_df.iterrows():
+#     #load PRFAUC table:
+#     method_name = row["Method"]
+#     preprocessing_hash = row["preprocessing_hash"]
+#     hyperparameter_hash = row["hyperparameter_hash"]
+#     PRFAUC_table_path = os.path.join(metric_folder, "PRFAUC_table", "Validation", method_name, preprocessing_hash)
+#     PRFAUC_table = load_table(PRFAUC_table_path, hyperparameter_hash)
+#     PRFAUC_table.loc["Average", :] = PRFAUC_table.mean()
     
-    index = [PRFAUC_table.index, [method_name]]
-    index = pd.MultiIndex.from_product([[method_name], PRFAUC_table.index], names=["Method", "Length category"])
+#     index = [PRFAUC_table.index, [method_name]]
+#     index = pd.MultiIndex.from_product([[method_name], PRFAUC_table.index], names=["Method", "Length category"])
     
-    PRFAUC_table.index = index
+#     PRFAUC_table.index = index
     
-    full_validation_results.append(PRFAUC_table)
-    #df_entry = 
-full_validation_results_df = pd.concat(full_validation_results).round(3)
-full_validation_results_df.rename(cutoff_replacement_dict, inplace=True)
-full_validation_results_df.rename(columns={"precision":"Precision", "recall":"Recall","ROC/AUC":"AUC"}, inplace=True)
+#     full_validation_results.append(PRFAUC_table)
+#     #df_entry = 
+# full_validation_results_df = pd.concat(full_validation_results).round(3)
+# full_validation_results_df.rename(cutoff_replacement_dict, inplace=True)
+# full_validation_results_df.rename(columns={"precision":"Precision", "recall":"Recall","ROC/AUC":"AUC"}, inplace=True)
 
 
-full_validation_results_df.to_latex(buf=os.path.join(table_folder, "full_validation_results.tex"), multirow=True)
+# full_validation_results_df.to_latex(buf=os.path.join(table_folder, "full_validation_results.tex"), multirow=True)
 
 #%% Visualize/tabularize segmentation results of different methods
-methods = {"SingleThresholdSPC":SingleThresholdStatisticalProcessControl, "SingleThresholdIF":SingleThresholdIsolationForest}
+# methods = {"SingleThresholdSPC":SingleThresholdStatisticalProcessControl, "SingleThresholdIF":SingleThresholdIsolationForest}
 
-preprocessing_overwrite=False 
-write_csv_intermediates=False
+# preprocessing_overwrite=False 
+# write_csv_intermediates=False
 
-which_split = "Validation"
-print("Split: Validation")
-X_val_dfs, y_val_dfs, X_val_files = load_batch(data_folder, which_split)
-val_file_names = X_val_files
+# which_split = "Validation"
+# print("Split: Validation")
+# X_val_dfs, y_val_dfs, X_val_files = load_batch(data_folder, which_split)
+# val_file_names = X_val_files
+
+# best_hyperparameters = {}
+# best_preprocessing_hyperparameters = {}
+# for method_name in methods:
+#     print("Now plotting: " + method_name)
+#     #find best preprocessing and method hyperparameters:
+
+#     #Some SQL query:
+    
+#     best_model_entry = db_cursor.execute("SELECT e.* FROM experiment_results e WHERE e.metric = (SELECT MAX(metric)FROM experiment_results WHERE method = (?) AND which_split = (?))", (method_name, "Test"))
+    
+#     (preprocessing_hash, hyperparameter_hash, _, _, preprocessing_hyperparameter_string_pickle, hyperparameter_string_pickle, _) = next(best_model_entry)
+    
+#     best_hyperparameters[method_name] = jsonpickle.decode(hyperparameter_string_pickle)
+#     best_preprocessing_hyperparameters[method_name] = jsonpickle.decode(preprocessing_hyperparameter_string_pickle)
+
+#     preprocessing_hyperparameters = best_preprocessing_hyperparameters[method_name]
+#     preprocessing_hyperparameter_string = str(preprocessing_hyperparameters)
+#     preprocessing_hash = sha256(preprocessing_hyperparameter_string.encode("utf-8")).hexdigest()
+    
+#     X_dfs_preprocessed, y_dfs_preprocessed, label_filters_for_all_cutoffs, event_lengths = preprocess_per_batch_and_write(X_val_dfs, y_val_dfs, intermediates_folder, which_split, preprocessing_overwrite, write_csv_intermediates, val_file_names, all_cutoffs, preprocessing_hyperparameters, preprocessing_hash)
+
+ 
+#     hyperparameters = best_hyperparameters[method_name] 
+#     hyperparameter_string = str(hyperparameters)
+    
+#     model = methods[method_name](model_folder, preprocessing_hash, **hyperparameters, score_function=score_function)
+#     model_name = model.method_name
+#     hyperparameter_hash = model.get_hyperparameter_hash()
+#     hyperparameter_hash_filename = model.get_filename()
+    
+#     base_scores_path = os.path.join(score_folder, which_split)
+#     base_predictions_path = os.path.join(predictions_folder, which_split)
+#     scores_path = os.path.join(base_scores_path, model_name, preprocessing_hash, hyperparameter_hash)
+#     predictions_path = os.path.join(base_predictions_path, model_name, preprocessing_hash, hyperparameter_hash)
+    
+#     y_pred_dfs, _ = load_dataframe_list(predictions_path)
+#     y_score_dfs, _ = load_dataframe_list(scores_path)
+ 
+#     plt.figure()
+#     plot_predictions(X_dfs_preprocessed, y_pred_dfs, val_file_names, model, pretty_plot=True, n_stations=1)
+#     plt.savefig(os.path.join(figure_folder, method_name+"_predictions.pdf"), format="pdf")
+#     plt.savefig(os.path.join(figure_folder, method_name+"_predictions.png"), format="png")
+
+#%% Bar plot of bootstrapping results on test:
+
+which_split = "Test"
+    
+methods = { "SingleThresholdIF":SingleThresholdIsolationForest,
+            "SingleThresholdBS":SingleThresholdBinarySegmentation, 
+            "SingleThresholdSPC":SingleThresholdStatisticalProcessControl,
+            
+            "DoubleThresholdBS":DoubleThresholdBinarySegmentation, 
+            "DoubleThresholdSPC":DoubleThresholdStatisticalProcessControl, 
+            
+            "Naive-SingleThresholdBS+SingleThresholdSPC":NaiveStackEnsemble, 
+            "Naive-DoubleThresholdBS+DoubleThresholdSPC":NaiveStackEnsemble,
+            "Naive-SingleThresholdBS+DoubleThresholdSPC":NaiveStackEnsemble,
+            "Naive-DoubleThresholdBS+SingleThresholdSPC":NaiveStackEnsemble,
+            
+            "SingleThresholdBS+SingleThresholdSPC":StackEnsemble, 
+            "DoubleThresholdBS+DoubleThresholdSPC":StackEnsemble, 
+            "SingleThresholdBS+DoubleThresholdSPC":StackEnsemble,
+            "DoubleThresholdBS+SingleThresholdSPC":StackEnsemble,
+            
+            "Sequential-SingleThresholdBS+SingleThresholdSPC":SequentialEnsemble, 
+            "Sequential-DoubleThresholdBS+DoubleThresholdSPC":SequentialEnsemble,
+            "Sequential-SingleThresholdBS+DoubleThresholdSPC":SequentialEnsemble,
+            "Sequential-DoubleThresholdBS+SingleThresholdSPC":SequentialEnsemble
+            }
 
 best_hyperparameters = {}
 best_preprocessing_hyperparameters = {}
 for method_name in methods:
-    print("Now plotting: " + method_name)
+    
     #find best preprocessing and method hyperparameters:
+    best_model_entry = db_cursor.execute("""
+    SELECT e.* 
+    FROM experiment_results e 
+    WHERE e.metric = (
+        SELECT MAX(metric)
+        FROM experiment_results
+        WHERE method = (?) AND which_split = (?)
+    ) AND e.method = (?)
+""", (method_name, "Validation", method_name))
 
-    #Some SQL query:
-    
-    best_model_entry = db_cursor.execute("SELECT e.* FROM experiment_results e WHERE e.metric = (SELECT MAX(metric)FROM experiment_results WHERE method = (?) AND which_split = (?))", (method_name, "Test"))
-    
     (preprocessing_hash, hyperparameter_hash, _, _, preprocessing_hyperparameter_string_pickle, hyperparameter_string_pickle, _) = next(best_model_entry)
-    
-    best_hyperparameters[method_name] = jsonpickle.decode(hyperparameter_string_pickle)
-    best_preprocessing_hyperparameters[method_name] = jsonpickle.decode(preprocessing_hyperparameter_string_pickle)
+    print(hyperparameter_hash)
+    best_hyperparameters[method_name] = jsonpickle.decode(hyperparameter_string_pickle, keys=True)
+    best_preprocessing_hyperparameters[method_name] = jsonpickle.decode(preprocessing_hyperparameter_string_pickle, keys=True)
 
-    preprocessing_hyperparameters = best_preprocessing_hyperparameters[method_name]
-    preprocessing_hyperparameter_string = str(preprocessing_hyperparameters)
-    preprocessing_hash = sha256(preprocessing_hyperparameter_string.encode("utf-8")).hexdigest()
-    
-    X_dfs_preprocessed, y_dfs_preprocessed, label_filters_for_all_cutoffs, event_lengths = preprocess_per_batch_and_write(X_val_dfs, y_val_dfs, intermediates_folder, which_split, preprocessing_overwrite, write_csv_intermediates, val_file_names, all_cutoffs, preprocessing_hyperparameters, preprocessing_hash)
-
- 
-    hyperparameters = best_hyperparameters[method_name] 
-    hyperparameter_string = str(hyperparameters)
-    
-    model = methods[method_name](model_folder, preprocessing_hash, **hyperparameters, score_function=score_function)
     model_name = model.method_name
-    hyperparameter_hash = model.get_hyperparameter_hash()
-    hyperparameter_hash_filename = model.get_filename()
-    
-    base_scores_path = os.path.join(score_folder, which_split)
-    base_predictions_path = os.path.join(predictions_folder, which_split)
-    scores_path = os.path.join(base_scores_path, model_name, preprocessing_hash, hyperparameter_hash)
-    predictions_path = os.path.join(base_predictions_path, model_name, preprocessing_hash, hyperparameter_hash)
-    
-    y_pred_dfs, _ = load_dataframe_list(predictions_path)
-    y_score_dfs, _ = load_dataframe_list(scores_path)
- 
-    plt.figure()
-    plot_predictions(X_dfs_preprocessed, y_pred_dfs, val_file_names, model, pretty_plot=True, n_stations=1)
-    plt.savefig(os.path.join(figure_folder, method_name+"_predictions.pdf"), format="pdf")
-    plt.savefig(os.path.join(figure_folder, method_name+"_predictions.png"), format="png")
+    PRF_mean_table_path = os.path.join(metric_folder, "PRF_mean_table", which_split, model_name, preprocessing_hash)
+    PRF_std_table_path = os.path.join(metric_folder, "PRF_std_table", which_split, model_name, preprocessing_hash)
+    avg_fbeta_mean_path = os.path.join(metric_folder, "bootstrap_mean_F"+str(beta), which_split, model_name, preprocessing_hash)
+    avg_fbeta_std_path = os.path.join(metric_folder, "bootstrap_std_F"+str(beta), which_split, model_name, preprocessing_hash)
